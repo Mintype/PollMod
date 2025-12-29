a0748e2041614f38bc43ada3023bd02fded517dc
Shahrian Masud <mintype.code@gmail.com>
2025-12-29 16:44:33 -0500

    Add initial files

---
+++ src/client/java/org/mintype/pollmod/client/PollmodClient.java
@@ -0,0 +1,10 @@
+package org.mintype.pollmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PollmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/mintype/pollmod/Pollmod.java
@@ -0,0 +1,10 @@
+package org.mintype.pollmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pollmod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
