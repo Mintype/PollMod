3cdc12e956a9a4be262916d24d3b97c59452ae17
Shahrian Masud <mintype.code@gmail.com>
2025-12-30 21:54:14 -0500

    Add more poll states

--- src/main/java/org/mintype/pollmod/poll/PollState.java
+++ src/main/java/org/mintype/pollmod/poll/PollState.java
@@ -1,6 +1,8 @@
 package org.mintype.pollmod.poll;
 
 public enum PollState {
+    UNSTARTED,     // Poll has not started yet
     ACTIVE,     // Poll is ongoing and players can vote
+    PAUSED,     // Poll is ongoing and players can vote
     ENDED,      // Poll has finished
 }
